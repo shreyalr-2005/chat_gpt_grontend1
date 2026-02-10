34205b78b29ab1ad413b094e11e374fddd6e9d19
Shreya LR <shravyashreya26@gmail.com>
2026-02-10 14:44:43 +0530

    do

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-      <h1>hello manasa  </h1>
+      <h1>hello manasa</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
