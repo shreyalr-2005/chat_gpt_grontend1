01ff9bb70348a1b2a231d876a630c288a6cc0999
Shreya LR <shravyashreya26@gmail.com>
2026-02-12 15:53:58 +0530

    do

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
